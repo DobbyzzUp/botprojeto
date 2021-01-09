6e70bb5a000f2429178ebd0381c56e5263969a9b
DobbyzzUp <76682818+DobbyzzUp@users.noreply.github.com>
2021-01-09 15:53:32 -0300

    Add files via upload

---
+++ src/darkmenu.js
@@ -0,0 +1,41 @@
+const dobbymenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  Comandos do 𝐷𝑂𝐵𝐵𝑌♱᭄:
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR 𝐷𝑂𝐵𝐵𝑌♱᭄
+  DUVIDAS? 👇
+  WA.me/5511911219615
+╚════════════════════`
+}
+
+exports.dobbymenu = dobbymenu
+
+
+
+
+
+
+
+
+
---
+++ src/help1.js
@@ -0,0 +1,46 @@
+const menu2 = (prefix) => {
+
+	return `
+
+➣COMANDOS DO 𝐷𝑂𝐵𝐵𝑌♱᭄
+
+
+
+
+➸ *${prefix}chamada*
+➸ *${prefix}chamada2*
+➸ *${prefix}chamada3*
+➸ *${prefix}loli*
+➸ *${prefix}loli1*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}akeno*
+➸ *${prefix}meme*
+➸ *${prefix}lofi*
+➸ *${prefix}malkova*
+➸ *${prefix}loli18*
+➸ *${prefix}reislin*
+➸ *${prefix}limparchat*
+➸ *${prefix}chamada*
+➸ *${prefix}ts (texto que deseja transmitir)*
+
+════════════════════
+⚡Digite ${prefix}dono para mais info⚡
+════════════════════`
+
+}
+exports.menu2 = menu2
+
+
+
